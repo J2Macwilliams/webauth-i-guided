906f84e14ca514530003582c5af91c3d0149d96b
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-16 11:18:42 -0800

    Fix login and register.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -23,10 +23,14 @@ user.password = hash;
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
 
+// check that the password is correct
+
+
+
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
